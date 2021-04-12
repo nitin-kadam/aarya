e0485b073f6393577b9c9b62202afc81b0470a5b
chavandeepak182 <62331741+chavandeepak182@users.noreply.github.com>
2021-04-12 16:13:15 +0530

    first

--- resources/views/backend/admin/layout/header.blade.php
+++ resources/views/backend/admin/layout/header.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ asset ('admin_media/production/images/favicon.ico')}}" type="image/ico" />
 
-    <title>Neurotherapy health ! </title>
+    <title>Deals Of Aarya </title>
 
     <!-- Bootstrap -->
     <link href="{{ asset('admin_media/vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
@@ -45,7 +45,7 @@
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="{{('dashboard')}}" class="site_title"><i class="fa fa-paw"></i> <span>CRM!</span></a>
+              <a href="{{('dashboard')}}" class="site_title"><i class="fa fa-paw"></i> <span>Deals Of Aarya</span></a>
             </div>
 
             <div class="clearfix"></div>
